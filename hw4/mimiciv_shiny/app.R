bfea6ce35ec6f435c9ad1ffacc82d13fd2b79202
Stefan Wang <wang@Stefans-Air.lan>
2024-03-08 20:46:22 -0800

    code style revised

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -19,16 +19,28 @@ con_bq <- dbConnect(
 
 
 #Data preparation
-sid_info <- tbl(con_bq, "patients") 
-sid_adt <- tbl(con_bq, "transfers") 
-sid_adm <- tbl(con_bq, "admissions") 
-sid_lab <- tbl(con_bq, "labevents") 
-sid_proc <- tbl(con_bq, "procedures_icd") 
-sid_dicd <- tbl(con_bq, "d_icd_procedures") 
-sid_proc <- sid_proc %>% left_join(sid_dicd, by = c("icd_code", "icd_version")) 
-sid_diag <- tbl(con_bq, "diagnoses_icd") 
-sid_didc <- tbl(con_bq, "d_icd_diagnoses") 
-sid_diag <- sid_diag %>% left_join(sid_didc, by = c("icd_code", "icd_version"))
+sid_info <- tbl(con_bq, 
+                "patients") 
+sid_adt <- tbl(con_bq, 
+               "transfers") 
+sid_adm <- tbl(con_bq, 
+               "admissions") 
+sid_lab <- tbl(con_bq, 
+               "labevents") 
+sid_proc <- tbl(con_bq, 
+                "procedures_icd") 
+sid_dicd <- tbl(con_bq, 
+                "d_icd_procedures") 
+sid_proc <- sid_proc %>% 
+  left_join(sid_dicd, 
+            by = c("icd_code", "icd_version")) 
+sid_diag <- tbl(con_bq, 
+                "diagnoses_icd") 
+sid_didc <- tbl(con_bq, 
+                "d_icd_diagnoses") 
+sid_diag <- sid_diag %>% 
+  left_join(sid_didc, 
+            by = c("icd_code", "icd_version"))
 
 mimic_icu_cohort <- readRDS("../mimiciv_shiny/mimic_icu_cohort.rds")
 
@@ -59,63 +71,136 @@ ui <- fluidPage(
                                        "Labevents" = "lab_measurements",
                                        "Vitals" = "vitals"
                            )),
-               checkboxInput("remove", "Remove outliers in IQR method for measurements?")
+               checkboxInput("remove", 
+                             "Remove outliers in IQR method for measurements?")
              ),
-             mainPanel(plotOutput("SummaryPlot"))
+             mainPanel(
+               plotOutput("SummaryPlot"))
            )
   ),
   tabPanel("Patient's ADT and ICU stay information", fluid = TRUE,
            sidebarLayout(
              sidebarPanel(
                helpText("Select a patient."),
-               selectizeInput("patientID", "Patient ID", choices = patient_id, multiple = FALSE)
+               selectizeInput("patientID", 
+                              "Patient ID", 
+                              choices = patient_id, 
+                              multiple = FALSE)
              ),
-             mainPanel(plotOutput("ADTPlot"))
+             mainPanel(
+               plotOutput("ADTPlot"))
            )
   )
   )
 )
 
 demographics <- mimic_icu_cohort %>%
-  select(first_careunit, last_careunit, los, admission_type, admission_location, discharge_location, insurance, language, marital_status, race, dod, gender, hospital_expire_flag)
+  select(first_careunit, 
+         last_careunit, 
+         los, 
+         admission_type, 
+         admission_location, 
+         discharge_location, 
+         insurance, 
+         language, 
+         marital_status, 
+         race, 
+         dod, 
+         gender, 
+         hospital_expire_flag)
 
 lab_measurements <- mimic_icu_cohort %>%
-  select(sodium, chloride, creatinine, potassium, glucose, hematocrit, wbc, bicarbonate)
+  select(sodium, 
+         chloride, 
+         creatinine, 
+         potassium, 
+         glucose, 
+         hematocrit, 
+         wbc, 
+         bicarbonate)
 
 vitals <- mimic_icu_cohort %>%
-  select(heart_rate, non_invasive_blood_pressure_systolic, non_invasive_blood_pressure_diastolic, respiratory_rate, temperature_fahrenheit)
+  select(heart_rate, 
+         non_invasive_blood_pressure_systolic, 
+         non_invasive_blood_pressure_diastolic, 
+         respiratory_rate, 
+         temperature_fahrenheit)
 
 
 server <- function(input, output, session) {
   
   reactiveData <- reactive({
     data <- switch(input$variable,
-                   "first_careunit" = mimic_icu_cohort %>% select(first_careunit) %>% na.omit() %>% collect(),
-                   "last_careunit" = mimic_icu_cohort %>% select(last_careunit) %>% na.omit() %>% collect(),
-                   "admission_type" = mimic_icu_cohort %>% select(admission_type) %>% na.omit() %>% collect(),
-                   "admission_location" = mimic_icu_cohort %>% select(admission_location) %>% na.omit() %>% collect(),
-                   "discharge_location" = mimic_icu_cohort %>% select(discharge_location) %>% na.omit() %>% collect(),
-                   "insurance" = mimic_icu_cohort %>% select(insurance) %>% na.omit() %>% collect(),
-                   "language" = mimic_icu_cohort %>% select(language) %>% na.omit() %>% collect(),
-                   "marital_status" = mimic_icu_cohort %>% select(marital_status) %>% na.omit() %>% collect(),
-                   "race" = mimic_icu_cohort %>% select(race) %>% na.omit() %>% collect(),
-                   "hospital_expire_flag" = mimic_icu_cohort %>% select(hospital_expire_flag) %>% na.omit() %>% collect(),
-                   "gender" = mimic_icu_cohort %>% select(gender) %>% na.omit() %>% collect(),
-                   "lab_measurements" = lab_measurements %>% pivot_longer(cols = everything(), names_to = "variable", values_to = "value"),
-                   "vitals" = vitals %>% pivot_longer(cols = everything(), names_to = "variable", values_to = "value")
+                   "first_careunit" = mimic_icu_cohort %>% 
+                     select(first_careunit) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "last_careunit" = mimic_icu_cohort %>% 
+                     select(last_careunit) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "admission_type" = mimic_icu_cohort %>% 
+                     select(admission_type) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "admission_location" = mimic_icu_cohort %>% 
+                     select(admission_location) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "discharge_location" = mimic_icu_cohort %>% 
+                     select(discharge_location) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "insurance" = mimic_icu_cohort %>% 
+                     select(insurance) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "language" = mimic_icu_cohort %>% 
+                     select(language) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "marital_status" = mimic_icu_cohort %>% 
+                     select(marital_status) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "race" = mimic_icu_cohort %>% 
+                     select(race) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "hospital_expire_flag" = mimic_icu_cohort %>% 
+                     select(hospital_expire_flag) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "gender" = mimic_icu_cohort %>% 
+                     select(gender) %>% 
+                     na.omit() %>% 
+                     collect(),
+                   "lab_measurements" = lab_measurements %>% 
+                     pivot_longer(cols = everything(), 
+                                  names_to = "variable", 
+                                  values_to = "value"),
+                   "vitals" = vitals %>% 
+                     pivot_longer(cols = everything(), 
+                                  names_to = "variable", 
+                                  values_to = "value")
     )
     
     # Check for outliers if requested
     if (input$remove) {
-      if ("value" %in% names(data) && is.numeric(data$value)) {
+      if ("value" %in% names(data) && 
+          is.numeric(data$value)) {
         # Apply the filter
         data <- data %>%
           filter(!is.na(value) & !is.infinite(value)) %>%  
           group_by(variable) %>%
           filter({
-            bounds <- quantile(value, probs = c(0.25, 0.75), na.rm = TRUE)
-            iqr_value <- IQR(value, na.rm = TRUE)
-            value > (bounds[1] - 1.5 * iqr_value) & value < (bounds[2] + 1.5 * iqr_value)
+            bounds <- quantile(value, 
+                               probs = c(0.25, 0.75), 
+                               na.rm = TRUE)
+            iqr_value <- IQR(value, 
+                             na.rm = TRUE)
+            value > (bounds[1] - 1.5 * iqr_value) & 
+              value < (bounds[2] + 1.5 * iqr_value)
           }) %>%
           ungroup()  
       }
@@ -129,20 +214,28 @@ server <- function(input, output, session) {
     data <- reactiveData()
     req(data)
     
-    is_categorical <- input$variable %in% names(demographics)
+    is_categorical <- input$variable %in% 
+      names(demographics)
     
     if (is_categorical) {
-      ggplot(data, aes_string(x = names(data))) +
+      ggplot(data, 
+             aes_string(x = names(data))) +
         geom_bar() +
         coord_flip() +
-        labs(title = "Patient count by variable", x = input$variable, y = "Count") +
+        labs(title = "Patient count by variable", 
+             x = input$variable, 
+             y = "Count") +
         theme_minimal()
     } else {
       # For numeric data, create appropriate plots
-      ggplot(data, aes(x = variable, y = value)) +
+      ggplot(data, 
+             aes(x = variable, 
+                 y = value)) +
         geom_boxplot() +
         coord_flip() +
-        labs(title = "Distribution of measurements", x = "", y = "Value") +
+        labs(title = "Distribution of measurements", 
+             x = "", 
+             y = "Value") +
         theme_minimal() 
     }
   })
@@ -156,12 +249,24 @@ reactiveData2 <- reactive({
   
   data_list <- list(
     sid = sid,
-    sid_info = sid_info %>% filter(subject_id == sid) %>% collect(),
-    sid_adt = sid_adt %>% filter(subject_id == sid) %>% collect(),
-    sid_adm = sid_adm %>% filter(subject_id == sid) %>% collect(),
-    sid_lab = sid_lab %>% filter(subject_id == sid) %>% collect(),
-    sid_proc = sid_proc %>% filter(subject_id == sid) %>% collect(),
-    sid_diag = sid_diag %>% filter(subject_id == sid) %>% collect()
+    sid_info = sid_info %>% 
+      filter(subject_id == sid) %>% 
+      collect(),
+    sid_adt = sid_adt %>% 
+      filter(subject_id == sid) %>% 
+      collect(),
+    sid_adm = sid_adm %>% 
+      filter(subject_id == sid) %>% 
+      collect(),
+    sid_lab = sid_lab %>% 
+      filter(subject_id == sid) %>% 
+      collect(),
+    sid_proc = sid_proc %>% 
+      filter(subject_id == sid) %>% 
+      collect(),
+    sid_diag = sid_diag %>% 
+      filter(subject_id == sid) %>% 
+      collect()
   )
   
   return(data_list)
@@ -196,8 +301,10 @@ reactiveData2 <- reactive({
       ) +
       geom_point(
         data = sid_lab %>%
-          distinct(charttime, .keep_all = TRUE),
-        mapping = aes(x = charttime, y = "Lab"),
+          distinct(charttime, 
+                   .keep_all = TRUE),
+        mapping = aes(x = charttime, 
+                      y = "Lab"),
         shape = '+',
         size = 5
       ) +
@@ -215,10 +322,12 @@ reactiveData2 <- reactive({
         title = str_c(
           "Patient ", sid, ", ",
           sid_info$gender, ", ",
-          sid_info$anchor_age + year(sid_adm$admittime[1]) - sid_info$anchor_year, " years old, ",
+          sid_info$anchor_age + year(sid_adm$admittime[1]) - sid_info$anchor_year, 
+          " years old, ",
           str_to_lower(sid_adm$race[1])
           ),
-        subtitle = str_c(str_to_lower(sid_diag$long_title[1:3]), collapse = "\n"),
+        subtitle = str_c(str_to_lower(
+          sid_diag$long_title[1:3]), collapse = "\n"),
         x = "Calendar Time",
         y = "",
         color = "Care Unit",
